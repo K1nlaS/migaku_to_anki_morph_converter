cd4f5305dd4b271831d3a1e2c348d100e107848d
Bohdan <stainedlake@gmail.com>
2023-09-18 17:34:34 +0300

    initial structure

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
 
 const isMac = process.platform === "darwin";
 const isDev = process.env.NODE_ENV !== "development";
@@ -22,12 +22,23 @@ const createMainWindow = () => {
 app.whenReady().then(() => {
   createMainWindow();
 
+  //Implement Menu
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+
   app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createMainWindow();
   });
 });
+
+// Menu Template
+const menu = [
+  {
+    role: "fileMenu"
+  }
+];
 
 app.on('window-all-closed', () => {
   if (!isMac) app.quit();
